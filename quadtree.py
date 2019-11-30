d4d3dee9701c63ada5497801d55464fd7538dd75
mkamadeus <mkamadeus.mka@gmail.com>
2019-11-30 23:34:47 +0700

    Added another image for testing

--- quadtree.py
+++ quadtree.py
@@ -97,7 +97,7 @@ def showImage(imageName):
     cv2.imshow("Lenna",imageName)
     cv2.waitKey(0)
 
-imageName = "lenna"
+imageName = "liechestein"
 imageName2 = "airplane"
 folderPath = "./result/"+imageName
 
@@ -108,21 +108,21 @@ height, width = imageInput.shape[0], imageInput.shape[1]
 
 print("Building quadtree representation of image...")
 Q = quadtree(imageInput, height, width)
-Q2 = quadtree(imageInput2, height, width)
+# Q2 = quadtree(imageInput2, height, width)
 
-showImage(imageUnion(Q,Q2,8))
-showImage(Q.scrambleImage(8))
+# showImage(imageUnion(Q,Q2,8))
+# showImage(Q.scrambleImage(8))
 
-print("Building quadtree representation of image...")
-Q3 = quadtree(Q.scrambleImage(8), height,width)
-showImage(Q3.scrambleImage(8))
+# print("Building quadtree representation of image...")
+# Q3 = quadtree(Q.scrambleImage(8), height,width)
+# showImage(Q3.scrambleImage(8))
 
 # Q.printTree(0)
-# os.mkdir(folderPath)
+os.mkdir(folderPath)
 
-# for i in range(1,10):
-#     image = Q.createLeveledCompression(Q, i)
-#     # showImage(image)
-#     cv2.imwrite(folderPath + "/" + str(1<<i) + "x" + str(1<<i) + ".png", image)
-#     print("Converted image of size " + str(1<<i) + "x" + str(1<<i))
+for i in range(1,10):
+    image = Q.createLeveledCompression(Q, i)
+    # showImage(image)
+    cv2.imwrite(folderPath + "/" + str(1<<i) + "x" + str(1<<i) + ".png", image)
+    print("Converted image of size " + str(1<<i) + "x" + str(1<<i))
 
